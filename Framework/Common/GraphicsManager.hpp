a6504585f2b65cab40ddef40579d78090663784f
hisky <573011406@qq.com>
2017-09-15 19:47:10 +0800

    gm

---
+++ Framework/Common/GraphicsManager.hpp
@@ -0,0 +1,10 @@
+#pragma once
+#include "IRuntimeModule.hpp"
+namespace Onion
+{
+    class GraphicsManager : implements IRuntimeModule
+    {
+    public:
+        virtual ~GraphicsManager(){}
+    };
+}
