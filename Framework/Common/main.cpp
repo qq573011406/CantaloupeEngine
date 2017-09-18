bc0cc14e995ed6cbd0ca123a903c46055d50089a
袁全伟 <qq573011406@126.com>
2017-09-18 19:51:24 +0800

    fix allocator crash bug.

--- Framework/Common/main.cpp
+++ Framework/Common/main.cpp
@@ -1,9 +1,13 @@
 #include <stdio.h>
 #include "IApplication.hpp"
+#include "GraphicsManager.hpp"
+#include "MemoryManager.hpp"
 using namespace Onion;
 namespace Onion
 {
 	extern IApplication* g_pApp;
+	extern GraphicsManager* g_pGraphicsManager;
+	extern MemoryManager* g_pMemoryManager;
 }
 
 int main(int argc,char** argv)
@@ -14,9 +18,25 @@ int main(int argc,char** argv)
 		printf("App Initialize failed,will exit now.");
 		return ret;
 	}
+
+	ret = g_pMemoryManager->Initialize();
+	if(ret!=0){
+		printf("MemoryManager Initialize failed,will exit now.");
+		return ret;
+	}
+
+	if ((ret = g_pGraphicsManager->Initialize()) != 0) {
+		printf("Graphics Manager Initialize failed, will exit now.");
+		return ret;
+	}	
+
 	while(!g_pApp->IsQuit()){
 		g_pApp->Tick();
+		g_pMemoryManager->Tick();
+		g_pGraphicsManager->Tick();
 	}
+	g_pGraphicsManager->Finalize();
+	g_pMemoryManager->Finalize();
 	g_pApp->Finalize();
 	return 0;
 }
